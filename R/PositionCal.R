499567483f4047e397c0af671f17df4c244a0f97
Zijin He <zijin.he@mail.utoronto.ca>
2020-11-17 11:52:57 -0500

    Upload PositionCal and rd files, README and DESCRIPTION

--- R/PositionCal.R
+++ R/PositionCal.R
@@ -1,1 +1,88 @@
-#, test for roxygen2 comment
+#' Find most conserved nucleotide in each position and its frequency
+#'
+#' This function grab the nucleotide for all of sequences at index position
+#' to create a XString sequence for a collection of this nucleotide. Then
+#' the function reformats the Xstring into a string clean it up.
+#'
+#' @param seqSet A set of aligned sequence read from fasta file
+#' @param index A numeric index for generating Position String at that position.
+#'
+#' @return A String containing nucleotides at index position for sequences.
+#'
+createPosVec <- function(seqSet, index){
+  postion <- c()
+
+  # Loop over each sequences and grab the nucleotide at index postion.
+  for (i in 1: length(seqSet)){
+    if(length(seqSet[[i]]) >= index){
+      nul <- seqSet[[i]][index]
+      nul <- paste(nul, "")
+      position[i] <- nul
+    }
+  }
+
+  # Reformat position from vector to a string and remove NA and whitespace
+  position <- paste(position, collapse = "")
+  position <- gsub(" ", "", position, fixed = TRUE)
+  position <- gsub("NA", "", position, fixed = TRUE)
+  return (position)
+}
+
+#' This function finds the highest frequency of nucleotide in input string.
+#'
+#' @param str A string containing character nucleotides
+#'
+#' @return  Return a numeric value for highest frequency
+#'
+findConservityFromS <- function(str){
+
+  # Split the str into character vector and find max occurances
+  # of certain nucleotide.
+  split <- strsplit(str, "")
+  maxval <- max(table(split))
+
+  # Use the length of the str to calculate the highest frequency
+  # of the nulceotide.
+  length <- nchar(str)
+  return(maxval / length)
+}
+
+#' This function finds the max length of sequence for input sequence set
+#'
+#' @param seqSet seqSet A set of aligned sequence read from fasta file
+#'
+#' @return A numeric value of the max length of sequence for seqSet
+
+findMaxLen <- function(seqSet){
+  max <- 0
+  for (i in 1:length(seqSet)){
+    if (max < length(seqSet[[i]])){
+      max <- length(seqSet[[i]])
+    }
+  }
+  return(max)
+}
+
+#' This function calculates the conservity of all positions for input
+#' sequences and generate a vector contains each position's conservity
+#'
+#' @param seqSet seqSet A set of aligned sequence read from fasta file
+#'
+#' @return a vector contains each position's
+#'
+conservityTable <- function(seqSet){
+  # Find max length of sequences in seqSet, it is used as the max range of
+  # position to compute the conservity. It also guarantees that each position
+  # to be calculated even the sequences have slightly length differences.
+  max <- findMaxLen(seqSet)
+
+  # From index 1 to max length, create a position string for each index
+  # and find the conservity from that index.
+  conserVec <- c()
+  for (i in 1:max){
+    newstr <- createPosVec(seqSet, i)
+    conser <- findConservityFromS(newstr)
+    conserVec[i] <- conser
+  }
+  return(conserVec)
+}
--- README.md
+++ README.md
@@ -7,53 +7,32 @@
 
 <!-- badges: end -->
 
-The goal of conservedPos is to …
+## Description
+
+Description: The goal of conservedPos package is to find important
+conserved sites for small target region by conducting a site-specific
+analysis for a set of aligned DNA sequences. The package will provide
+function to count nucleotides occurrances in each position independently
+and calculates the frequency of the nucleotides in order to quantify the
+conservity of each position.
 
 ## Installation
 
-You can install the released version of conservedPos from
-[CRAN](https://CRAN.R-project.org) with:
+You can install the released version of conservedPos
 
 ``` r
 install.packages("conservedPos")
-```
 
-And the development version from [GitHub](https://github.com/) with:
-
-``` r
 # install.packages("devtools")
-devtools::install_github("hezijin/conservedPos")
+library(devtools)
+require
+
+devtools::install_github("hezijin/conservedPos", build_vignettes = TRUE)
 ```
 
 ## Example
 
-This is a basic example which shows you how to solve a common problem:
+## Acknowledgements
 
-``` r
-library(conservedPos)
-## basic example code
-```
-
-What is special about using `README.Rmd` instead of just `README.md`?
-You can include R chunks like so:
-
-``` r
-summary(cars)
-#>      speed           dist       
-#>  Min.   : 4.0   Min.   :  2.00  
-#>  1st Qu.:12.0   1st Qu.: 26.00  
-#>  Median :15.0   Median : 36.00  
-#>  Mean   :15.4   Mean   : 42.98  
-#>  3rd Qu.:19.0   3rd Qu.: 56.00  
-#>  Max.   :25.0   Max.   :120.00
-```
-
-You’ll still need to render `README.Rmd` regularly, to keep `README.md`
-up-to-date.
-
-You can also embed plots, for example:
-
-<img src="man/figures/README-pressure-1.png" width="100%" />
-
-In that case, don’t forget to commit and push the resulting figure
-files, so they display on GitHub\!
+This package was developed as part of an assessment for 2020BCB410H:
+Applied Bioinformatics, University of Toronto, Toronto,CANADA.
